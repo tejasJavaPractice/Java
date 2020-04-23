646801f44ec494eb83f376e3f86f59e27a64db07
Tejas Jethva <tejasmjethva@gmail.com>
2020-04-23 07:07:30 +0530

    Uncommented code

--- SpringCore/src/main/java/com/spring/core/SpringMain.java
+++ SpringCore/src/main/java/com/spring/core/SpringMain.java
@@ -10,21 +10,21 @@ public class SpringMain
 		//ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
 		
-//		HelloWorld helloWorld = (HelloWorld) context.getBean("helloWorld");
-//		
-//		System.out.println(helloWorld.getMessage());
-//		
-//		Employee emp = (Employee) context.getBean("employee");
-//		
-//		System.out.println(emp.getName());
-//		
-//		System.out.println(emp.getTransportService().getTransportMode());
-//		
-//		System.out.println("Hashcode of employee: " + emp.hashCode());
-//		
-//		Employee emp2 = (Employee) context.getBean("employee");
-//		
-//		System.out.println("Hashcode of employee: " + emp2.hashCode());
+		HelloWorld helloWorld = (HelloWorld) context.getBean("helloWorld");
+		
+		System.out.println(helloWorld.getMessage());
+		
+		Employee emp = (Employee) context.getBean("employee");
+		
+		System.out.println(emp.getName());
+		
+		System.out.println(emp.getTransportService().getTransportMode());
+		
+		System.out.println("Hashcode of employee: " + emp.hashCode());
+		
+		Employee emp2 = (Employee) context.getBean("employee");
+		
+		System.out.println("Hashcode of employee: " + emp2.hashCode());
 		
 		Driver driver = context.getBean(Driver.class);
 		
